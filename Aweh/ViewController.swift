fd7139003b430dea3f5636c3fc4a91b680b81ba7
Divine <divine.dube@takealot.com>
2020-06-02 18:49:08 +0200

    Initial Commit

---
+++ Aweh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aweh
+//
+//  Created by Divine.Dube on 2020/06/02.
+//  Copyright © 2020 com.github.aweh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
