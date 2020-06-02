3df88af305e05f33c02894956608c32e94d95729
Divine <divine.dube@takealot.com>
2020-06-03 00:12:08 +0200

    Create main viewController

--- Aweh/HomeScreen/ViewController/ViewController.swift
+++ Aweh/HomeScreen/ViewController/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
